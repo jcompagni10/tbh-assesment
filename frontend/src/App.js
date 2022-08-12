21d6f7053ceb7bab2993d1455a91ca101941a6e2
Julian Compagni Portis <julian@terrascope.io>
2022-08-12 00:32:04 -0700

    ADD react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Link } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+          <h1> TBH Demo App </h1>
+          <Link to ="/form/123"> Sample Form </Link>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/form.js
@@ -0,0 +1,142 @@
+pfsuimport React from 'react';
+import {useParams } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
+
+
+
+
+
+function RadioQuestion (props){
+    let {label, type, values} = props.question;
+
+    return (q
+        <div>
+            <h3> {label} </h3>
+            <RadioGroup>
+                {values.map( value => {
+                    return <FormControlLabel key={value} value={value} control={<Radio onChange= {() => props.onChange(value)} />} label={value} />
+                }
+                )}
+            </RadioGroup>
+        </div>
+    )
+
+}
+
+function TextQuestion (props){
+    let {label, type, values} = props.question;
+
+    return (
+        <div>
+            <h3> {label} </h3>
+            <TextField id="outlined-basic" variant="outlined" onChange={(e)=>props.onChange(e.target.value)}
+                       rows={4} multiline={true} sx = {{width: "500px"}}/>
+        </div>
+    )
+
+}
+
+function CheckboxQuestion (props){
+    let {label, type, values} = props.question;
+    let checkedAnswers = new Set();
+    let onChange = function (checked, val){
+        if (checked){
+            checkedAnswers.add(val)
+        } else{
+            checkedAnswers.delete(val)
+        }
+        props.onChange(checkedAnswers)}
+
+    return (
+        <div>
+            <h3> {label} </h3>
+            <div>
+                {values.map( value => {
+                    return <FormControlLabel key={value}
+                                             value={value}
+                                             control={<Checkbox onChange= {(e, checked) => onChange(checked, value)} />} label={value} />
+                }
+                           )}
+            </div>
+        </div>
+    )
+}
+
+function FormQuestionIndex({questions, responses}){
+    let onChange = function (idx, value) {
+        responses[idx]=value
+        console.log(responses);
+    }
+
+    return (
+        <div>
+            {questions.map((question, idx) => {
+                switch(question.type){
+                case "radio":
+                    return <RadioQuestion key={question.id} question={question} onChange={onChange.bind(null, idx)}/>;
+
+                case "text":
+                    return <TextQuestion key={question.id} question={question} onChange={onChange.bind(null, idx)}/>;
+
+                case "checkbox":
+                    return <CheckboxQuestion key={question.id} question={question} onChange={onChange.bind(null, idx)}/>;
+                }
+            })}
+            <TextField id="outlined-basic" label="Email" variant="outlined"
+                       onChange={(e)=>onChange("email", e.target.value)}
+                       sx = {{mt: "12px", width: "500px"}} />
+        </div>
+    );}
+
+class FormWrapper extends React.Component {
+    constructor(props) {
+        super(props);
+        let formID = props.formID;
+        this.state = {formID, responses: {}};
+    }
+
+    submitForm(){
+        console.log("Submit:", JSON.stringify(this.state.responses));
+        fetch("http://localhost:9000/form/", {method: "POST",
+                                              headers: {
+                                                  'Content-Type': 'application/json'},
+                                              body: JSON.stringify(this.state.responses)})
+    }
+
+    componentDidMount (){
+        fetch("http://localhost:9000/form/"+this.state.formID )
+            .then(resp=>resp.json()
+                  .then(body=>
+                      this.setState({form: body})));
+    }
+
+    render (){
+        if (this.state.form) {
+            return (<div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                        <h2> Post-Session Form </h2>
+                        <FormQuestionIndex questions={this.state.form} responses={this.state.responses}/>
+
+                        <Button variant="contained" sx={{width: "200px", mt: "20px"}} onClick={this.submitForm.bind(this)}>Submit</Button>
+                    </div>);
+                   }
+            else{
+                return <h3> Loading Form ... </h3>;
+            }
+
+    }
+}
+
+
+export default function Form() {
+    let {formID} = useParams();
+    return(
+        <main style={{ padding: "1rem 0" }}>
+            <FormWrapper formID={formID}/>
+        </main>
+    );
+}
